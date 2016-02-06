e06a009f147a180623aaaa454f5fbc1b2d9b9d75
ceckenrode <chriseckenrode@me.com>
2016-02-06 11:14:26 -0500

    added views, added node server to serve these assets. testing to see if they can be pushed to heroku

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var PORT = 8080;
+
+app.use("/js", express.static("public/js"));
+app.use("/css", express.static("public/css"));
+
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/home.html');
+});
+
+app.get('/registration', function(req, res) {
+  res.sendFile(process.cwd() + '/views/registration.html');
+});
+
+app.get('/admin', function(req, res) {
+  res.sendFile(process.cwd() + '/views/admin.html');
+});
+
+app.listen(PORT, function(){
+  console.log('Listening on port %s', PORT);
+})
