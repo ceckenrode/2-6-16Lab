e2c257d6930c4605364acee3e41ac7caede04a66
ceckenrode <chriseckenrode@me.com>
2016-02-06 11:33:19 -0500

    fixed another syntax error in line 3 of server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var PORT = process.env.PORT 8080;
+var PORT = process.env.port || 8080;
 
 app.use("/js", express.static("public/js"));
 app.use("/css", express.static("public/css"));
